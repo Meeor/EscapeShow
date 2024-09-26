5c51e7552195c500ad517ccc75ba26e4f20915af
Rion4966 <phila3926@naver.com>
2024-09-27 01:13:43 +0900

    플레어건 콘솔명령어및 아머스탠드소환 제거(탈출구현으로인해 불필요해짐)

--- src/main/kotlin/kr/rion/plugin/item/FlameGunActions.kt
+++ src/main/kotlin/kr/rion/plugin/item/FlameGunActions.kt
@@ -31,12 +31,6 @@ object FlameGunActions {
         val particleData1 = Particle.DustOptions(Color.fromRGB(255, 0, 0), 1.5f)
         val particleData2 = Particle.DustOptions(Color.fromRGB(255, 0, 0), 1.0f)
 
-        val console: CommandSender = Bukkit.getConsoleSender()
-        val cmd1 =
-            "summon minecraft:armor_stand ${initialLoc.x} ${initialLoc.y} ${initialLoc.z} {Tags:[helicop],Invisible:1b}" // 실행할 명령어
-        val cmd2 = "function server_datapack:flare"
-        Bukkit.dispatchCommand(console, cmd1)
-
         for (onlinePlayer in Bukkit.getOnlinePlayers()) {
             object : BukkitRunnable() {
                 private var t = 0.0
@@ -88,7 +82,6 @@ object FlameGunActions {
                             particleData2,
                             true
                         )
-                        Bukkit.dispatchCommand(console, cmd2)
                         Bukkit.getScheduler().runTaskLater(Loader.instance, Runnable {
                             Helicopter.spawn(initialLoc.clone().add(0.0, 50.0, 0.0))
                             startEscape(player)
