f223aa9ecd791fe9a4a8b653e0041dd9b2a8908a
Rion4966 <phila3926@naver.com>
2024-10-02 01:53:16 +0900

    텔레포트오브젝트에있던 무적적용함수를 이곳으로 이동

--- src/main/kotlin/kr/rion/plugin/event/DamageEvent.kt
+++ src/main/kotlin/kr/rion/plugin/event/DamageEvent.kt
@@ -1,16 +1,16 @@
 package kr.rion.plugin.event
 
+import kr.rion.plugin.util.Teleport.immunePlayers
 import org.bukkit.GameMode
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
-import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDamageEvent
 
 class DamageEvent : Listener {
     //탈출한 플레이어가 받는 모든데미지 무시
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     fun onDamage(event: EntityDamageEvent) {
         if (event.entity is Player) {
             val player = event.entity as Player
@@ -20,13 +20,15 @@ class DamageEvent : Listener {
         }
     }
 
-    //탈출한 플레이어가 받는 플레이어공격및엔티티공격 전부무시
+    //텔레포트시 잠시동안 무적상태
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    fun onPlayerDamageByEntity(event: EntityDamageByEntityEvent) {
+    fun onPlayerDamage(event: EntityDamageEvent) {
         if (event.entity is Player) {
             val player = event.entity as Player
-            if (player.gameMode == GameMode.ADVENTURE && player.scoreboardTags.contains("EscapeComplete")) {
-                event.isCancelled = true // 다른 플레이어의 공격도 무효화
+            val currentTime = System.currentTimeMillis()
+            // 플레이어가 면역 상태인지 확인
+            if (immunePlayers[player]?.let { it > currentTime } == true) {
+                event.isCancelled = true // 면역 상태일 경우 데미지 무시
             }
         }
     }
