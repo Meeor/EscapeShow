c928c562020d979fb2c017b2d9002b62c5199f77
Rion4966 <phila3926@naver.com>
2024-10-03 00:21:35 +0900

    보이스챗 api에 필요한 누락된던 이벤트등록 추가

--- src/main/kotlin/kr/rion/plugin/event/VoiceChatEvent.kt
+++ src/main/kotlin/kr/rion/plugin/event/VoiceChatEvent.kt
@@ -2,6 +2,7 @@ package kr.rion.plugin.event
 
 import de.maxhenkel.voicechat.api.VoicechatApi
 import de.maxhenkel.voicechat.api.VoicechatPlugin
+import de.maxhenkel.voicechat.api.events.EventRegistration
 import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent
 import org.bukkit.Bukkit
 
@@ -14,6 +15,11 @@ class VoiceChatEvent : VoicechatPlugin {
         // 여기서 필요한 초기화 작업을 수행할 수 있습니다.
         // 예를 들어 로그를 남기거나, API를 사용한 추가 작업 등을 설정합니다.
         Bukkit.getLogger().info("VoiceChat API가 초기화되었습니다!")
+    }
+
+    // 반드시 이벤트 등록!
+    override fun registerEvents(registration: EventRegistration) {
+        registration.registerEvent(MicrophonePacketEvent::class.java, this::onMicrophonePacket)
     }
 
     fun onMicrophonePacket(event: MicrophonePacketEvent) {
