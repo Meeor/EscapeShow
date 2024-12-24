9dbbc66b5a40f90faba7713181c66814b5a04e90
Rion4966 <phila3926@naver.com>
2024-12-24 23:00:42 +0900

    디버그메세지 제거

--- src/main/kotlin/kr/rion/plugin/event/PlayerMoveListener.kt
+++ src/main/kotlin/kr/rion/plugin/event/PlayerMoveListener.kt
@@ -39,36 +39,4 @@ class PlayerMoveListener : Listener {
             })
         }
     }
-
-    //////플라이 풀림 원인 디버깅
-    /*@EventHandler
-    fun onGameModeChange(event: PlayerGameModeChangeEvent) {
-        val player = event.player
-        if (event.newGameMode != GameMode.CREATIVE && event.newGameMode != GameMode.SPECTATOR) {
-            Bukkit.broadcastMessage("${player.name}님의 플라이가 해제되었습니다. ${ChatColor.YELLOW}사유 : 게임 모드 변경됨")
-            Exception().printStackTrace() // 스택 트레이스를 바로 콘솔에 출력
-        }
-    }
-
-    private val notifiedPlayers = mutableSetOf<Player>() // 이미 메시지를 출력한 플레이어 목록
-
-    @EventHandler
-    fun onPlayerFlyDisableCheck(event: PlayerMoveEvent) {
-        val player = event.player
-
-        // EscapeComplete 태그가 있는 플레이어만 체크
-        if (player.scoreboardTags.contains("EscapeComplete")) {
-            // allowFlight 상태가 false이고 아직 메시지를 출력하지 않은 경우
-            if (!player.allowFlight && !notifiedPlayers.contains(player)) {
-                Bukkit.getLogger().info("${player.name}의 플라이 상태가 해제되었습니다. ${ChatColor.YELLOW}MoveEvent 에서 확인됨.")
-                notifiedPlayers.add(player) // 메시지를 출력한 플레이어로 추가
-            }
-
-            // allowFlight가 다시 true로 설정되면 목록에서 제거
-            if (player.allowFlight) {
-                notifiedPlayers.remove(player)
-            }
-        }
-    }*/
-    /////플라이 풀림 원인 디버깅
 }
