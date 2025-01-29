4b66364ec17a411e945e016e80985c2438380b2d
Rion4966 <phila3926@naver.com>
2025-01-29 22:36:48 +0900

    미션 순서 수정됨.

--- src/main/kotlin/kr/rion/plugin/mission/MissionList.kt
+++ src/main/kotlin/kr/rion/plugin/mission/MissionList.kt
@@ -23,20 +23,20 @@ object MissionList {
         MissionRegistry.registerMission(13, KillWithSpecificItemMission(Material.AIR)) //PlayerDeathEvent
         MissionRegistry.registerMission(14, KillWithSpecificItemMission(Material.COBBLESTONE)) //PlayerDeathEvent
         MissionRegistry.registerMission(15, CumulativeDamageMission(20.0)) //EntityDamageByEntityEvent
-        MissionRegistry.registerMission(16, CumulativeDamageTakenMission(10.0)) //EntityDamageByEntityEvent
-        MissionRegistry.registerMission(17, ItemCollectMission(Material.CHAINMAIL_CHESTPLATE, 1)) //inventoryCloseEvent
-        MissionRegistry.registerMission(18, CraftingMission(listOf(Material.IRON_CHESTPLATE to 1))) //inventoryClickEvent
-        MissionRegistry.registerMission(19, CraftingMission(listOf(Material.DIAMOND_CHESTPLATE to 1))) //inventoryClickEvent
-        MissionRegistry.registerMission(20, ReviveOnceMission())
-        MissionRegistry.registerMission(21, StealItemsMission())
-        MissionRegistry.registerMission(22, ItemCollectMission(Material.LEATHER, 30)) //inventoryCloseEvent
-        MissionRegistry.registerMission(23,CumulativeHealingMission(10.0, Loader.instance)) //PlayerInteractEvent
-        MissionRegistry.registerMission(24, ItemUsageMission(Material.SKULL_BANNER_PATTERN, "contract", 1, Loader.instance)) //PlayerInteractEvent
-        MissionRegistry.registerMission(25, ItemUsageMission(Material.MOJANG_BANNER_PATTERN, "map", 3, Loader.instance)) //PlayerInteractEvent
-        MissionRegistry.registerMission(26, ItemUsageMission(Material.NETHER_BRICK, "gips", 1, Loader.instance)) //PlayerInteractEvent
-        MissionRegistry.registerMission(27, ItemUsageMission(Material.PAPER, "heal", 10, Loader.instance)) //PlayerInteractEvent
-        MissionRegistry.registerMission(28, ItemUsageMission(Material.PAPER, "heal", 15, Loader.instance)) //PlayerInteractEvent
-        MissionRegistry.registerMission(29, ItemUsageMission(Material.PAPER, "heal", 20, Loader.instance)) //PlayerInteractEvent
+        MissionRegistry.registerMission(16, ItemCollectMission(Material.CHAINMAIL_CHESTPLATE, 1)) //inventoryCloseEvent
+        MissionRegistry.registerMission(17, CraftingMission(listOf(Material.IRON_CHESTPLATE to 1))) //inventoryClickEvent
+        MissionRegistry.registerMission(18, CraftingMission(listOf(Material.DIAMOND_CHESTPLATE to 1))) //inventoryClickEvent
+        MissionRegistry.registerMission(19, ReviveOnceMission())
+        MissionRegistry.registerMission(20, StealItemsMission())
+        MissionRegistry.registerMission(21, ItemCollectMission(Material.LEATHER, 30)) //inventoryCloseEvent
+        MissionRegistry.registerMission(22, CumulativeHealingMission(10.0, Loader.instance)) //PlayerInteractEvent
+        MissionRegistry.registerMission(23, ItemUsageMission(Material.SKULL_BANNER_PATTERN, "contract", 1, Loader.instance)) //PlayerInteractEvent
+        MissionRegistry.registerMission(24, ItemUsageMission(Material.MOJANG_BANNER_PATTERN, "map", 3, Loader.instance)) //PlayerInteractEvent
+        MissionRegistry.registerMission(25, ItemUsageMission(Material.NETHER_BRICK, "gips", 1, Loader.instance)) //PlayerInteractEvent
+        MissionRegistry.registerMission(26, ItemUsageMission(Material.PAPER, "heal", 10, Loader.instance)) //PlayerInteractEvent
+        MissionRegistry.registerMission(27, ItemUsageMission(Material.PAPER, "heal", 15, Loader.instance)) //PlayerInteractEvent
+        MissionRegistry.registerMission(28, ItemUsageMission(Material.PAPER, "heal", 20, Loader.instance)) //PlayerInteractEvent
+        MissionRegistry.registerMission(29, CumulativeDamageTakenMission(10.0)) //EntityDamageByEntityEvent
         MissionRegistry.registerMission(30, DamageFreeMission(600, Loader.instance))
         MissionRegistry.registerMission(31, LastSurvivorMission())
 
