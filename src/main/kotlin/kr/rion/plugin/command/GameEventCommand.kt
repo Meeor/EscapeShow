e3a387d962828f097a8df6cef88e8c9e10cc225f
Rion4966 <phila3926@naver.com>
2025-02-08 22:03:45 +0900

    이벤트 제거

--- src/main/kotlin/kr/rion/plugin/command/GameEventCommand.kt
+++ src/main/kotlin/kr/rion/plugin/command/GameEventCommand.kt
@@ -16,12 +16,6 @@ object GameEventCommand {
         when (args[0].lowercase(Locale.getDefault())) {
             "맑음" -> GameEvent.weatherClear()
             "폭우" -> GameEvent.weatherRain()
-            "중력이상" -> GameEvent.gravity()
-            "지진" -> GameEvent.earthQuake(sender)
-            "후원" -> GameEvent.donation()
-            "데스코인" -> GameEvent.deathCoin()
-            "베팅" -> GameEvent.betting()
-            "랜덤" -> GameEvent.randomEvent(sender)
             else -> sender.sendMessage("$prefix ${ChatColor.RED} 이벤트이름을 정확히 입력해주세요 (자동완성 지원됩니다.)")
         }
     }
--- src/main/kotlin/kr/rion/plugin/gameEvent/GameEvent.kt
+++ src/main/kotlin/kr/rion/plugin/gameEvent/GameEvent.kt
@@ -1,17 +1,7 @@
 package kr.rion.plugin.gameEvent
 
 
-import org.bukkit.Bukkit
-import org.bukkit.command.CommandSender
-
-
 object GameEvent {
-    private val survivalPlayers = Bukkit.getOnlinePlayers().filter { player ->
-        !player.scoreboardTags.contains("manager") && !player.scoreboardTags.contains("EscapeComplete") && !player.scoreboardTags.contains(
-            "death"
-        )
-    }
-
 
     fun weatherClear() {
         WeatherClear.applyClearWeather()
@@ -19,42 +9,6 @@ object GameEvent {
 
     fun weatherRain() {
         WeatherRain.applyRainWeather()
-    }
-
-    fun gravity() {
-        Gravity.applyGravityAnomaly(survivalPlayers)
-    }
-
-    fun earthQuake(sender: CommandSender) {
-        EarthQuake.start(sender)
-    }
-
-    fun donation() {
-        Donation.applyDonationEvent()
-    }
-
-    fun deathCoin() {
-        DeathCoin.applyDeathCoinEvent()
-    }
-
-    fun betting() {
-        Betting.applyBettingEvent()
-    }
-
-    fun randomEvent(sender: CommandSender) {
-        // 함수들을 리스트로 저장
-        val eventFunctions = listOf(
-            { weatherClear() },
-            { weatherRain() },
-            { gravity() },
-            { earthQuake(sender) },
-            { donation() },
-            { deathCoin() },
-            { betting() }
-        )
-
-        // 무작위로 하나의 함수를 선택하여 호출
-        eventFunctions.random().invoke()
     }
 
 }
