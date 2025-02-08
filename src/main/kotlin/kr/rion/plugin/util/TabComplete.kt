f4ccdd958452c70824fd2555538ac671ca37fcba
Rion4966 <phila3926@naver.com>
2025-02-08 22:25:48 +0900

    이벤트 자동완성 "맑음" 과 " 폭우" 를 제외하고 모두 제거

--- src/main/kotlin/kr/rion/plugin/util/TabComplete.kt
+++ src/main/kotlin/kr/rion/plugin/util/TabComplete.kt
@@ -20,7 +20,6 @@ class TabComplete : TabCompleter {
             "이벤트" -> GameEvent(args)
             "미션" -> HandleMissionTab(args)
             "방송" -> HandleBroadcastTab(args)
-            "부활불가" -> HandleReviveTab(args)
             else -> null
         }
     }
@@ -61,16 +60,6 @@ class TabComplete : TabCompleter {
             2 -> listOf("true", "false").filter { it.startsWith(args[1], ignoreCase = true) }
             else -> emptyList()
         }
-    }
-    private fun HandleReviveTab(args: Array<out String>): List<String> {
-        // 명령어 입력 중 첫 번째 인수라면 플레이어 이름을 자동완성으로 반환
-        if (args.size == 1) {
-            val input = args[0].lowercase() // 사용자가 입력한 값을 소문자로 변환
-            return Bukkit.getOnlinePlayers()
-                .map { it.name } // 모든 온라인 플레이어의 이름을 가져옴
-                .filter { it.lowercase().startsWith(input) } // 입력값과 일치하는 이름만 필터링
-        }
-        return emptyList() // 그 외에는 자동완성 결과 없음
     }
 
 }
