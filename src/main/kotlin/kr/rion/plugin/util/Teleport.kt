daed41fcb44c4242a3e0af72f9a7cc7e7363debb
Rion4966 <phila3926@naver.com>
2025-02-19 23:58:00 +0900

    무적 작업 시작작업에서 일괄적으로 관리하도록 수정하였고. 텔레포트를 팀단위로 이동되도록 수정, 텔레포트위치찾는작업을 시작작업시에 찾는것으로 수정

--- src/main/kotlin/kr/rion/plugin/util/Teleport.kt
+++ src/main/kotlin/kr/rion/plugin/util/Teleport.kt
@@ -1,14 +1,16 @@
 package kr.rion.plugin.util
 
 import kr.rion.plugin.Loader
+import kr.rion.plugin.manager.TeamManager
 import kr.rion.plugin.manager.WorldManager
 import kr.rion.plugin.util.Global.prefix
 import net.md_5.bungee.api.ChatColor
-import org.bukkit.*
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.World
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.potion.PotionEffect
-import org.bukkit.potion.PotionEffectType
 import java.util.*
 
 
@@ -24,9 +26,8 @@ object Teleport {
     private var designatedWorld: World? = null
     private var destinationWorld: World? = null
     val console = Bukkit.getServer().consoleSender
-    val immunePlayers = mutableMapOf<Player, Long>() // 플레이어와 면역 시간 맵
+    val immunePlayers = mutableSetOf<Player>() // 플레이어와 면역 시간 맵
     val stopPlayer: MutableMap<Player, Boolean> = mutableMapOf()
-    val tpstopPlayer: MutableMap<Player, Boolean> = mutableMapOf()
 
     private var hasInitializedSafeLocations = false
 
@@ -66,12 +67,13 @@ object Teleport {
         val radius = 326 // 반경 326 블록
         val minY = 53  // 최소 Y 좌표
         val maxY = 97  // 최대 Y 좌표
+        val teamcount = TeamManager.getTeamCount()
 
-        val requiredSafeLocations = 60 // 목표: 60개
+        val requiredSafeLocations = 5 + teamcount // 목표: 60개
         val maxAttempts = 1000000 // 시도 횟수 제한 (필요에 따라 조정 가능)
         var attempts = 0
 
-        val minDistance = 15.0 // 최소 거리 설정 (30 블록)
+        val minDistance = radius * 1.5 / teamcount // 최소 거리 설정 (30 블록)
 
         while (safeLocations.size < requiredSafeLocations && attempts < maxAttempts) {
             // 원형 범위 안에서 무작위 좌표 생성
@@ -91,7 +93,7 @@ object Teleport {
         }
 
         val endTime = System.currentTimeMillis()
-        console.sendMessage("$prefix 안전한 좌표 ${ChatColor.YELLOW}${safeLocations.size} ${ChatColor.GREEN}개를 찾았습니다. 걸린시간 : ${ChatColor.LIGHT_PURPLE}${endTime - startTime}ms")
+        Bukkit.broadcastMessage("$prefix 팀들이 이동될 좌표를 찾았습니다. 걸린시간 : ${ChatColor.LIGHT_PURPLE}${endTime - startTime}ms")
         hasInitializedSafeLocations = true
     }
 
@@ -107,58 +109,64 @@ object Teleport {
                 loc.z in -40.0..40.0
     }
 
-    fun teleportToRandomLocation(player: Player) {
+    fun teleportTeamToRandomLocation(players: List<Player>) {
         if (!hasInitializedSafeLocations) {
-            player.sendMessage("$prefix 안전한 좌표가 초기화되지 않았습니다. 나중에 다시 시도해주세요.")
+            players.forEach { it.sendMessage("$prefix 안전한 좌표가 초기화되지 않았습니다. 나중에 다시 시도해주세요.") }
             return
         }
 
         val startTime = System.currentTimeMillis()
-
         val safeLocation = safeLocations.randomOrNull()
-        val targetChunk = safeLocation?.chunk
-        if (targetChunk != null) {
-            if (!targetChunk.isLoaded) {
-                targetChunk.load()  // 청크 강제 로드
-            }
-        }
+
         if (safeLocation == null) {
             console.sendMessage("$prefix 좌표를 찾을 수 없습니다. 재설정을 해주세요.")
-            player.sendMessage("$prefix 저장된 좌표값이 없어 이동에 실패하였습니다.\n$prefix 운영자에게 좌표설정을 부탁하시길 바랍니다.")
+            players.forEach { it.sendMessage("$prefix 저장된 좌표값이 없어 이동에 실패하였습니다.\n$prefix 운영자에게 좌표설정을 부탁하시길 바랍니다.") }
             return
         }
 
-        // 텔레포트 상태 확인
-        val isTeleporting = tpstopPlayer.getOrDefault(player, false)
-        if (isTeleporting) {
-            player.sendMessage("$prefix 이미 텔레포트 작업이 진행 중입니다.")
-            return
+        val targetChunk = safeLocation.chunk
+        if (!targetChunk.isLoaded) {
+            targetChunk.load()  // 청크 강제 로드
         }
-        tpstopPlayer[player] = true // 텔레포트 상태 설정
 
-        // 메인 스레드에서 텔레포트 작업 수행
         Bukkit.getScheduler().runTask(Loader.instance, Runnable {
             try {
-                setImmune(player) // 텔레포트 후 면역 처리
-                player.teleport(safeLocation)
-                // 텔레포트 완료 후 작업 처리
-                safeLocations.remove(safeLocation) // 성공 시 좌표 제거
+                for (player in players) {
+                    // 반경 5칸 내 랜덤 위치 선정
+                    val offsetX = (-5..5).random()
+                    val offsetZ = (-5..5).random()
+                    val candidateLocation = safeLocation.clone().add(offsetX.toDouble(), 0.0, offsetZ.toDouble())
+
+                    // 하늘에서부터 안전한 지점 찾기
+                    var teleportLocation: Location? = null
+                    for (y in 255 downTo 50) {
+                        val checkLocation = candidateLocation.clone().apply { this.y = y.toDouble() }
+                        if (isLocationCaveAir(checkLocation) && checkLocation.block.type == Material.AIR) {
+                            teleportLocation = checkLocation
+                            break
+                        }
+                    }
+
+                    if (teleportLocation != null) {
+                        player.teleport(teleportLocation)
+                    } else {
+                        console.sendMessage("$prefix ${player.name}의 안전한 좌표를 찾을 수 없어 기본 위치로 이동합니다.")
+                        player.teleport(safeLocation)
+                    }
+                }
+
+                // 텔레포트 완료 후 중앙 위치 제거
+                safeLocations.remove(safeLocation)
 
                 val endTime = System.currentTimeMillis()
-                console.sendMessage("$prefix 플레이어 텔레포트 지연시간: ${endTime - startTime}ms")
-                player.addPotionEffect(PotionEffect(PotionEffectType.BLINDNESS, 999999, 1, false, false))
-                Bukkit.getScheduler().runTaskLater(Loader.instance, Runnable {
-                    player.playSound(player, "custom.start", SoundCategory.MASTER, 1.0f, 1.0f)
-                    player.sendTitle("${ChatColor.GREEN}게임을 시작합니다.", "${ChatColor.YELLOW}상대를 죽이고 탈출수단을 이용해서 이곳을 탈출하세요.")
-                    stopPlayer[player] = true
-                }, 30)
+                console.sendMessage("$prefix 팀 텔레포트 지연시간: ${endTime - startTime}ms")
+
             } catch (e: Exception) {
                 console.sendMessage("$prefix 텔레포트 중 오류가 발생했습니다: ${e.message}")
-            } finally {
-                tpstopPlayer[player] = false // 텔레포트 상태 해제
             }
         })
     }
+
 
 
     private fun isLocationCaveAir(loc: Location): Boolean {
@@ -167,13 +175,6 @@ object Teleport {
         return block.type == Material.CAVE_AIR
     }
 
-    private fun setImmune(player: Player) {
-        val currentTime = System.currentTimeMillis()
-        immunePlayers[player] = currentTime + 5000
-        Bukkit.getScheduler().runTaskLater(plugin, Runnable {
-            immunePlayers.remove(player)
-        }, (5000 / 50))
-    }
 
     fun setInitializedSafeLocations(status: Boolean) {
         hasInitializedSafeLocations = status
