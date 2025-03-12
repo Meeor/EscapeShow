0f2b7cecc412b7085f56f1efd98d580935e951bf
Rion4966 <phila3926@naver.com>
2025-03-12 20:02:16 +0900

    커스텀 이벤트 추가

---
+++ src/main/kotlin/kr/rion/plugin/customEvent/IronMeltEvent.kt
@@ -0,0 +1,19 @@
+package kr.rion.plugin.customEvent
+
+import org.bukkit.entity.Player
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+import org.bukkit.inventory.ItemStack
+
+class IronMeltEvent(
+    val player: Player,
+    val item: ItemStack
+): Event() {
+    companion object{
+        private val handlers = HandlerList()
+
+        @JvmStatic
+        fun getHandlerList() = handlers
+    }
+    override fun getHandlers(): HandlerList = handlers
+}
