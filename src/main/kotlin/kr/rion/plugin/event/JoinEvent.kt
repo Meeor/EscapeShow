cdb6c5742c444a98a73dff3c56bbfaa8f4b21cde
Rion4966 <phila3926@naver.com>
2025-03-14 22:56:03 +0900

    입장메세지 필요없어보여서 제거. 우벌님한테 말하면 알아서 쉐이더 자동적용 해주실것으로 예상중..

--- src/main/kotlin/kr/rion/plugin/event/JoinEvent.kt
+++ src/main/kotlin/kr/rion/plugin/event/JoinEvent.kt
@@ -17,28 +17,6 @@ class JoinEvent : Listener {
     //접속 메세지
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     fun onjoin(event: PlayerJoinEvent) {
-        object : BukkitRunnable() {
-            override fun run() {
-                // 비동기 작업에서 플레이어에게 인사 메시지 전송
-
-
-                event.player.sendMessage("${ChatColor.GOLD}$line\n\n")
-                centerText(
-                    """
-                    |${ChatColor.YELLOW}왁타버스 마인크래프트 조공 컨텐츠
-                    |${ChatColor.GREEN}Escape Show ${ChatColor.YELLOW}에 오신것을 환영합니다.
-                    |
-                    |${ChatColor.AQUA}Esc 키 -> 설정 -> 비디오 설정 -> 쉐이더팩 설정에서
-                    |${ChatColor.LIGHT_PURPLE}Bliss.zip
-                    |${ChatColor.AQUA}을 적용해주시길 바랍니다.
-                    |
-                    |${ChatColor.YELLOW}※TIP※ : 채팅창이 거슬린다면
-                    |${ChatColor.YELLOW}키보드 F3 키와 D 키를 동시에 눌러주세요
-                    """.trimMargin()
-                    )
-                event.player.sendMessage("${ChatColor.GOLD}$line")
-            }
-        }.runTask(Loader.instance)
         Bukkit.getScheduler().runTaskLater(Loader.instance, Runnable {
             if (event.player.scoreboardTags.contains("EscapeComplete") ||
                 event.player.scoreboardTags.contains("death") ||
